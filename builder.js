7832b8dad4af9543b0816b72f263bef395009548
Rudolf Klikh <Rudolf.Klikh@AllScripts.com>
2023-06-05 21:16:39 +0200

    Practice

---
+++ builder.js
@@ -0,0 +1,29 @@
+class Tag {
+    constructor(name = '', text = '') {
+        this.name = name;
+        this.text = text;
+        this.children = [];
+    } 
+}
+
+
+const hello = 'hello';
+let html = [];
+
+html.push('<p>');
+html.push(hello);
+html.push('</p>');
+
+console.log(html.join(''));
+
+const words = ['hello', 'world'];
+
+html = [];
+
+html.push('<ul>\n');
+for (let word of words) {
+    html.push(`<li>${word}</li>\n`);
+}
+html.push('</ul>');
+
+console.log(html.join(''))
